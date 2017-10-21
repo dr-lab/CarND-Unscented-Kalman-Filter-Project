4f089bc57ce33a7e0fb3dfd5b8115fb2b92f43f4
Huaping Gu <huapinggu@Huapings-Home-MacBook-Pro.local>
2017-10-21 16:42:33 -0700

    hw check in after first local build success and integartion with simulator

---
+++ UnscentedKF.build/Debug/ALL_BUILD.build/Script-551DFBEF21304B02B2E1ACA5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/huapinggu/Sources/NDCar/term2/CarND-Unscented-Kalman-Filter-Project -f /Users/huapinggu/Sources/NDCar/term2/CarND-Unscented-Kalman-Filter-Project/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
